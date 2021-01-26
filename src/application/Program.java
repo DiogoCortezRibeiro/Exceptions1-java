0cd44e29cadb840429997cfa98e12a42c55356d0
DiogoCortezRibeiro <diogocortezr@gmail.com>
2021-01-26 17:59:49 -0300

    Good soluction

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,23 +6,24 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entites.Reservation;
+import models.excepetions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("Check-in (DD/MM/YYYY): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out (DD/MM/YYYY): ");
-		Date checkOut = sdf.parse(sc.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date!!");
-		}else {
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in (DD/MM/YYYY): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out (DD/MM/YYYY): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+			System.out.println();
+			
 			Reservation reserva = new Reservation(number, checkIn, checkOut);
 			System.out.println(reserva);
 			System.out.println("Enter data to update the reservation");
@@ -30,14 +31,17 @@ public class Program {
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out (DD/MM/YYYY): ");
 			checkOut = sdf.parse(sc.next());
-			String error = reserva.updateDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println("Error in reservation: " + error);
-				
-			}else {
-				System.out.println(reserva);
-			}
+			
+			System.out.println();
+			
+			reserva.updateDates(checkIn, checkOut);
+			System.out.println(reserva);
+		}catch(ParseException e) {
+			System.out.println("Invalid date format!!");
+		}catch(DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
 		}
+			
 		sc.close();
 	}
 
